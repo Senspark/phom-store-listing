ab08e4b47bdeb264f287be65d8cacbb48da1451e
enrevol <enrevol@gmail.com>
2016-10-28 14:47:09 +0700

    - update script.

--- itunes/populate_metadata.py
+++ itunes/populate_metadata.py
@@ -4,6 +4,6 @@
 import sys
 import os, os.path
 
-cmd = "python ../../../store-listing-toolkit/populate-v3.py metadata -platform iOS -prj-path . -sheet 15WJ6dtUsROCkjmMaNj-BMp7CjOiFjB4B7MPD5hoHzq8 -customized-metadata-path ../src/itunes/metadata"
+cmd = "python ../../../store-listing-toolkit/populate-v3.py metadata -platform iOS -prj-path . -sheet 1cFx92LErkPfHNbuljaWceJZdA6Kerg0gUcuJXN42ctc -customized-metadata-path ../src/itunes/metadata"
 print cmd
 os.system(cmd)
